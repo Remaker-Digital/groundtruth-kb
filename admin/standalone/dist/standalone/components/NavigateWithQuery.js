e27006b02c9f2b40be2bfa2f7e19f434a5a96af4
Remaker Digital <mike@remakerdigital.com>
2026-03-24 18:20:35 -0700

    bump: v1.98.21 + build.py auto-bumps PRODUCT_VERSION from tag

--- admin/standalone/dist/standalone/components/NavigateWithQuery.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Navigate, useLocation } from 'react-router-dom';
-export const NavigateWithQuery = ({ to, replace }) => {
-    const location = useLocation();
-    const target = to.includes('?') ? to : `${to}${location.search}`;
-    return _jsx(Navigate, { to: target, replace: replace });
-};
-//# sourceMappingURL=NavigateWithQuery.js.map
